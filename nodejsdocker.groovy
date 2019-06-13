ed7f214f215a0f3c50cabaa7bb0daca78b447ae5
samarth0157 <samarth3112@live.com>
2019-06-13 13:47:20 +0530

    Create nodejsdocker.groovy

---
+++ nodejsdocker.groovy
@@ -0,0 +1,26 @@
+job('NodeJS Docker Job created by DSL') {
+    scm {
+        git('git://github.com/samarth0157/demo-nodejs-app.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('SAMARTH')
+            node / gitConfigEmail('samarth3112@live.com')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('mynodes') // this is the name of the NodeJS installation in 
+                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+    }
+    steps {
+        dockerBuildAndPublish {
+            repositoryName('sam0157/jenkins-nodeimage')
+            tag('${GIT_REVISION,length=9}')
+            registryCredentials('dockerhub')
+            forcePull(false)
+            forceTag(false)
+            createFingerprints(false)
+            skipDecorate()
+        }
+    }
+}
